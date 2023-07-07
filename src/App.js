8a3d0492cc701846538252d1eb9283e8bb457e16
younghoonyou <kcnoh2@hanyang.ac.kr>
2023-07-06 19:38:28 -0700

    update gitignore

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import './assets/css/App.css';
-import { useEffect } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import "./assets/css/App.css";
+import { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function setScreenSize() {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 }
 
-
 function App() {
   useEffect(() => {
     setScreenSize();
   });
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
