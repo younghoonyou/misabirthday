807976d01714584cbe42163bfe0c1f3c32cf0870
younghoonyou <kcnoh2@hanyang.ac.kr>
2023-07-16 18:53:29 -0700

    Ready to deploy

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import {createRoot} from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import ScrollToTop from './lib/scrolltoTop';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <ScrollToTop>
       <App />
     </ScrollToTop>
