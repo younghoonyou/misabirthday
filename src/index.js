0b44b8ab16626a4927b902b9b87fc222d34b6ccb
younghoonyou <kcnoh2@hanyang.ac.kr>
2023-07-16 22:13:19 -0700

    ready to deploy on phone

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import ScrollToTop from './lib/scrolltoTop';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <ScrollToTop>
       <App />
     </ScrollToTop>
-  </BrowserRouter>
+  </HashRouter>
 );
