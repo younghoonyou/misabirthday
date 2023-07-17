242e223f6346424c0083b4989ed716a0d543929e
younghoonyou <kcnoh2@hanyang.ac.kr>
2023-07-16 22:17:12 -0700

    modify router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import {BrowserRouter, HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import ScrollToTop from './lib/scrolltoTop';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <ScrollToTop>
       <App />
     </ScrollToTop>
-  </HashRouter>
+  </BrowserRouter>
 );
